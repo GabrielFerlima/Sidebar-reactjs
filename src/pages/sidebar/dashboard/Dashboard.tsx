9dabe8905d0731f078c1b8ff7434b1df1b5082d9
GabrielFerlima <gabriel_fernandes1995@hotmail.com>
2022-10-31 21:55:18 -0300

    Ajuste de icones e layout

--- src/pages/sidebar/dashboard/Dashboard.tsx
+++ src/pages/sidebar/dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 import './styles.css'
 import logo from '../../../assets/logo.png'
-import { CaretRight, HouseLine, MagnifyingGlass } from 'phosphor-react'
+import { Bell, CaretRight, ChartBar, ChartPie, Heart, HouseLine, MagnifyingGlass, Moon, SignOut, Sun, Wallet } from 'phosphor-react'
 export function Dashboard() {
     return (
         <nav className='sidebar'>
@@ -22,18 +22,73 @@ export function Dashboard() {
             <div className='menubar'>
                 <div className='menu'>
                     <li className='searchLink'>
-                        <span className='search'> <MagnifyingGlass size={20} color="#707070" /> </span>
+                        <span className='search'> <MagnifyingGlass size={20}  /> </span>
                         <input type="search" placeholder='Search...' />
                     </li>
 
                     <ul className='menuLink'>
                         <li className='navLink'>
                             <a href="#">
-                                <span className='icon'><HouseLine size={20} color="#707070" /></span>
+                                <span className='icon'><HouseLine size={20}  /></span>
                                 <span className='navText'>Dashboard</span>
                             </a>
                         </li>
+
+                        <li className='navLink'>
+                            <a href="#">
+                                <span className='icon'><ChartBar size={20} /></span>
+                                <span className='navText'>Revenue</span>
+                            </a>
+                        </li>
+
+                        <li className='navLink'>
+                            <a href="#">
+                                <span className='icon'><Bell size={20} /></span>
+                                <span className='navText'>Notifications</span>
+                            </a>
+                        </li>
+
+                        <li className='navLink'>
+                            <a href="#">
+                                <span className='icon'><ChartPie size={20} /></span>
+                                <span className='navText'>Analytics</span>
+                            </a>
+                        </li>
+
+                        <li className='navLink'>
+                            <a href="#">
+                                <span className='icon'><Heart size={20}/></span>
+                                <span className='navText'>Likes</span>
+                            </a>
+                        </li>
+
+                        <li className='navLink'>
+                            <a href="#">
+                                <span className='icon'><Wallet size={20}/></span>
+                                <span className='navText'>Wallets</span>
+                            </a>
+                        </li>
                     </ul>
+                </div>
+
+                <div className='bottomContent'>
+                <li className='navLink'>
+                            <a href="#">
+                                <span className='icon'><SignOut size={20} /></span>
+                                <span className='navText'>Logout</span>
+                            </a>
+                        </li>
+
+                        <li className='mode'>
+                           <div className='moonSun'>
+                           <Moon size={20} />
+                           <Sun size={20} />
+                           </div>
+                           <span className='modeText'>Dark Mode</span>
+                           <div className='toggleSwitch'>
+                            <span className='switch'></span>
+                           </div>
+                        </li>
                 </div>
 
             </div>
